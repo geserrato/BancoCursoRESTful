9a63dafdd88ebd79dddaabadb07034d9b6ef17f1
Gerardo Estrella <gerardo.estrella@sygno.com.mx>
2022-03-13 16:27:30 -0600

    Avance en dbcontext repository

--- Application/Features/Clients/Commands/CreateClientCommand/CreateClientCommand.cs
+++ Application/Features/Clients/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Application.Features.Clients.Commands.CreateClientCommand;
 
-public class CreateClientCommand : IRequest<Response<int>>
+public abstract class CreateClientCommand : IRequest<Response<int>>
 {
     public string Name { get; set; }
     public string Surname { get; set; }
@@ -11,4 +11,12 @@ public class CreateClientCommand : IRequest<Response<int>>
     public string Phone { get; set; }
     public string Email { get; set; }
     public string Address { get; set; }
+}
+
+public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Response<int>>
+{
+    public async Task<Response<int>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Persistence.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly IDateTimeService _dateTimeService;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeService dateTimeService) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        _dateTimeService = dateTimeService;
+    }
+
+    public DbSet<Client> Clients { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (EntityEntry<AuditableBaseEntity> entry in ChangeTracker.Entries<AuditableBaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Modified:
+                    entry.Entity.Created = _dateTimeService.NowUtc;
+                    break;
+                case EntityState.Added:
+                    entry.Entity.LastModified = _dateTimeService.NowUtc;
+                    break;
+                case EntityState.Detached:
+                case EntityState.Unchanged:
+                case EntityState.Deleted:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
